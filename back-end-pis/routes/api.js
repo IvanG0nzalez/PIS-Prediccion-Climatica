a904194a8c888ada22c463604ee34dea422764d6
Davud404 <david25ale03@gmail.com>
2024-01-03 17:17:31 -0500

    Controlador y routes para sensores

--- back-end-pis/routes/api.js
+++ back-end-pis/routes/api.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const sensorC = require('../app/controls/SensorControl');
+let sensorControl = new sensorC();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//api sensores
+router.get('/admin/sensores', sensorControl.listar);
+router.post('/admin/sensores/guardar', sensorControl.guardar);
+router.get('/admin/sensores/obtener/:external', sensorControl.obtener_sensor);
+router.patch('/admin/sensores/modificar/:external', sensorControl.modificar);
+router.get('/admin/sensores/obtener/historial_climatico/:external', sensorControl.obtener_historial_climatico);
+router.get('/admin/sensores/obtener/prediccion_climatica/:external', sensorControl.obtener_prediccion_climatica);
 module.exports = router;
