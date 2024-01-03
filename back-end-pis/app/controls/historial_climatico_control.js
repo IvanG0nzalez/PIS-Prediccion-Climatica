7ba0e2cf72825e1a7ba63de27ae85ba91644c887
Jhandry <jhandrychimbo@gmail.com>
2024-01-03 17:51:30 -0500

    Actualizacion Historial

--- back-end-pis/app/controls/historial_climatico_control.js
+++ back-end-pis/app/controls/historial_climatico_control.js
@@ -5,18 +5,70 @@ var historial = models.historial_climatico;
 var sensor = models.sensor;
 
 class HistorialControl {
-  async listar(req, res) {
-    var lista = await historial.findAll({
-      include: [
-        {
-          model: models.sensor,
-          as: "sensor",
-          attribute: ["external_id", "alias", "ip", "tipo_medicion"],
-        },
-      ],
-      attributes: ["fecha", ["external_id", "id"], "hora", "valor_medio"],
-    });
-    res.status(200);
-    res.json({ msg: "OK", code: 200, datos: lista });
-  }
+
+    //LISTAR HISTORIAL
+    async listar(req, res) {
+        var lista = await historial.findAll({
+            include: [
+                {
+                    model: models.sensor,
+                    as: "sensor",
+                    attributes: ["external_id", "alias", "ip", "tipo_medicion"],
+                },
+            ],
+            attributes: ["fecha", "external_id", "hora", "valor_medio"],
+        });
+        res.status(200);
+        res.json({ msg: "OK", code: 200, datos: lista });
+    }
+
+    //GUARDAR HISTORIAL
+    async guardar(req, res) {
+        if (
+            req.body.hasOwnProperty("fecha") &&
+            req.body.hasOwnProperty("hora") &&
+            req.body.hasOwnProperty("valor_calculado") &&
+            req.body.hasOwnProperty("sensor")
+        ) {
+            var uuid = require("uuid");
+
+            var sensorA = await sensor.findOne({
+                where: { external_id: req.body.sensor },
+            });
+
+            if (sensorA == undefined || sensorA == null) {
+                res.status(401);
+                res.json({
+                    msg: "ERROR",
+                    tag: "El sensor a buscar no existe",
+                    code: 401,
+                });
+            } else {
+                var data = {
+                    fecha: req.body.fecha,
+                    external_id: uuid.v4(),
+                    hora: req.body.hora,
+                    valor_calculado: req.body.valor_calculado,
+                    sensor: req.body.sensor,
+                };
+
+                var result = await historial.create(data);
+                if (result === null) {
+                    res.status(401);
+                    res.json({ msg: "Error", tag: "No se puede crear", code: 401 });
+                } else {
+                    res.status(200);
+                    res.json({ msg: "OK", code: 200 });
+                }
+            }
+
+
+        } else {
+            res.status(400);
+            res.json({ msg: "ERROR", tag: "Faltan datos", code: 400 });
+        }
+    }
+
 }
+
+module.exports = HistorialControl;
--- back-end-pis/routes/api.js
+++ back-end-pis/routes/api.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const historialC = require("../app/controls/historial_climatico_control");
+let historialControl = new historialC();
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+
+//HISTORIAL 
+router.get("/admin/historial", historialControl.listar);
+router.post("/admin/historial/save", historialControl.guardar);
+
 
 module.exports = router;
