396490926b6e4a43a48a5ebbe2b53ee6a5199442
IvanG0nzalez <ivanalegonzalez3@gmail.com>
2024-01-04 13:13:53 -0500

    Metodo predictivo y schedule para predicciones

--- back-end-pis/routes/api.js
+++ back-end-pis/routes/api.js
@@ -3,6 +3,9 @@ var router = express.Router();
 const sensorC = require('../app/controls/SensorControl');
 let sensorControl = new sensorC();
 
+const prediccionC = require('../app/controls/PrediccionClimaticaControl');
+let prediccionControl = new prediccionC();
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -15,4 +18,9 @@ router.get('/admin/sensores/obtener/:external', sensorControl.obtener_sensor);
 router.patch('/admin/sensores/modificar/:external', sensorControl.modificar);
 router.get('/admin/sensores/obtener/historial_climatico/:external', sensorControl.obtener_historial_climatico);
 router.get('/admin/sensores/obtener/prediccion_climatica/:external', sensorControl.obtener_prediccion_climatica);
+
+
+//api prediccion climatica
+router.post('/predicciones/guardar', prediccionControl.guardar);
+
 module.exports = router;
