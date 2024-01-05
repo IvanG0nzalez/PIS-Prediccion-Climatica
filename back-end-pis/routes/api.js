77db41909ebc1857a21469244ade8925329e05a8
DavidIntriago <david.intriago@unl.edu.ec>
2024-01-05 18:34:15 -0500

    Middleware

--- back-end-pis/routes/api.js
+++ back-end-pis/routes/api.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+
+let jwt = require("jsonwebtoken");
+
+
 const sensorC = require('../app/controls/SensorControl');
 let sensorControl = new sensorC();
 
@@ -26,6 +30,122 @@ router.get('/', function (req, res, next) {
 });
 
 
+//MIDDLEWARE
+const auth = function middleware(req, res, next) {
+  const token = req.headers["token-key"];
+
+  console.log(req.headers);
+
+  if (token === undefined) {
+    res.status(401);
+    res.json({
+      msg: "ERROR",
+      tag: "Falta token",
+      code: 401,
+    });
+  } else {
+    require("dotenv").config();
+    const key = process.env.KEY;
+    jwt.verify(token, key, async (err, decoded) => {
+      if (err) {
+        res.status(401);
+        res.json({
+          msg: "ERROR",
+          tag: "Token no valido o expirado",
+          code: 401,
+        });
+      } else {
+        req.id = decoded.external;
+        console.log("aquio");
+        console.log(req.id);
+        const models = require("../app/models");
+        const cuenta = models.cuenta;
+        const aux = await cuenta.findOne({
+          where: { external_id: decoded.external },
+        });
+        if (aux === null) {
+          res.status(401);
+          res.json({
+            msg: "ERROR",
+            tag: "Token no valido",
+            code: 401,
+          });
+        } else {
+          next();
+        }
+      }
+    });
+  }
+  // console.log(req.url);
+  // console.log(token);
+  // next();
+};
+
+const isSuperAdmin = async (req, res, next) => {
+  const models = require("../app/models");
+  const cuenta = models.cuenta;
+  const cuentaAux = await cuenta.findOne({
+    where: { external_id: req.id },
+  });
+  const usuario = models.usuario;
+  const usuarioAux = await usuario.findOne({
+    where: { id: cuentaAux.id_usuario },
+  });
+  const rol = models.rol;
+  console.log("aqui ajdo")
+  const rolAux = await rol.findOne({
+    where: { id: usuarioAux.id_rol },
+  });
+
+  if ((rolAux.nombre = "Super-Administrador")) {
+    next();
+  } else {
+    res.status(401);
+    res.json({
+      msg: "ERROR",
+      tag: "Debe ser un Super Administrador",
+      code: 401,
+    });
+  }
+
+  // console.log(req.url);
+  // console.log(token);
+  // next();
+};
+
+
+const isAdmin = async (req, res, next) => {
+  const models = require("../app/models");
+  const cuenta = models.cuenta;
+  const cuentaAux = await cuenta.findOne({
+    where: { external_id: req.id },
+  });
+  const usuario = models.usuario;
+  const usuarioAux = await usuario.findOne({
+    where: { id: cuentaAux.id_usuario },
+  });
+  const rol = models.rol;
+  const rolAux = await rol.findOne({
+    where: { id: usuarioAux.id_rol },
+  });
+
+  if ((rolAux.nombre = "Administrador")) {
+    next();
+  } else {
+    res.status(401);
+    res.json({
+      msg: "ERROR",
+      tag: "Debe ser un Super Admin",
+      code: 401,
+    });
+  }
+
+  // console.log(req.url);
+  // console.log(token);
+  // next();
+};
+
+
 //api historial climatico 
 router.get("/admin/historiales", historialControl.listar);
 router.post("/admin/historiales/guardar", historialControl.guardar);
@@ -47,17 +167,19 @@ router.post('/predicciones/guardar', prediccionControl.guardar);
 
 
 //ROL
-router.get("/admin/roles", rolControl.listar);
+router.get("/admin/roles",[auth, isSuperAdmin], rolControl.listar);
 router.post("/admin/rol/guardar", rolControl.guardar);
 
 //USUARIO
 router.get("/admin/usuarios", usuarioControl.listar);
 router.post("/admin/usuario/guardar", usuarioControl.crear);
+router.patch("/admin/usuario/modificar/:external", usuarioControl.modificar);
 
 //CUENTA
 router.post("/admin/inicio_sesion", cuentaControl.inicio_sesion);
 router.get("/admin/cuentas", cuentaControl.listar);
-router.patch("/admin/cuenta/modificar/:external", cuentaControl.update)
+router.patch("/admin/cuenta/estado/:external", cuentaControl.actualizar_estado);
+
 
 
 
