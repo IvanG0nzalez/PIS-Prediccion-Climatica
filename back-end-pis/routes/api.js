8e4f6b34a325c4338fbf2c2c1de7c28d5c048f06
DavidIntriago <david.intriago@unl.edu.ec>
2024-01-07 14:17:37 -0500

    ArreglosGET, calculo_error, auths

--- back-end-pis/routes/api.js
+++ back-end-pis/routes/api.js
@@ -167,21 +167,23 @@ router.get('/predicciones', prediccionControl.obtener_proximas_4);
 
 //ROL
 router.get("/admin/roles",[auth, isSuperAdmin], rolControl.listar);
-router.post("/admin/roles/guardar", rolControl.guardar);
+router.post("/admin/roles/guardar",[auth, isSuperAdmin], rolControl.guardar);
 
 //USUARIO
 router.get("/admin/usuarios", usuarioControl.listar);
-router.post("/admin/usuarios/guardar", usuarioControl.guardar);
-router.patch('/admin/usuarios/modificar/:external', usuarioControl.modificar);
+router.get("/admin/usuarios/obtener/:external", usuarioControl.obtener);
+router.post("/admin/usuarios/guardar", [auth, isSuperAdmin],usuarioControl.guardar);
+router.patch('/admin/usuarios/modificar/:external',[auth, isSuperAdmin], usuarioControl.modificar);
 
 //CUENTA
-router.post("/admin/inicio_sesion", cuentaControl.inicio_sesion);
-router.get("/admin/cuentas", cuentaControl.listar);
-router.patch("/admin/cuentas/estado/:external", cuentaControl.actualizar_estado)
+router.post("/admin/inicio_sesion",auth, cuentaControl.inicio_sesion);
+router.post("/admin/cuentas/clave/:external",auth, cuentaControl.cambiar_Clave);
+router.get("/admin/cuentas", [auth, isSuperAdmin], cuentaControl.listar);
+router.patch("/admin/cuentas/estado/:external",[auth, isSuperAdmin],  cuentaControl.actualizar_estado)
 
 //REPORTEEE
 router.get("/admin/reporte", prediccionControl.reporte);
-router.get("/admin/clima", prediccionControl.weather);
+//router.get("/admin/clima", prediccionControl.weather);
 
 
 
