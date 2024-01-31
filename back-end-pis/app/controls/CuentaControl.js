cdb3d9f075e3e33ae610c11c39a254ff46a76b0e
IvanG0nzalez <ivanalegonzalez3@gmail.com>
2024-01-30 19:31:17 -0500

    Validaciones, Arreglos Controladores y Rutas

--- back-end-pis/app/controls/CuentaControl.js
+++ back-end-pis/app/controls/CuentaControl.js
@@ -8,25 +8,8 @@ var rol = models.rol;
 var usuario = models.usuario;
 var cuenta = models.cuenta;
 let jwt = require("jsonwebtoken");
-/*
-instalar
-npm install jsonwebtoken --save
-npm install doteenv --save
-*/
 
 class CuentaControl {
-  async listar(req, res) {
-    var lista = await cuenta.findAll({
-      attributes: ["nombre_usuario", "clave", "estado", "external_id"],
-    });
-    res.status(200);
-    res.json({
-      msg: "OK",
-      code: 200,
-      datos: lista,
-    });
-  }
-
   async actualizar_estado(req, res) {
     const external_usuario = req.params.external;
 
@@ -35,35 +18,26 @@ class CuentaControl {
         external_id: external_usuario,
       }
     });
-    var cuentaAux=await cuenta.findOne({
-        where: {
-            id_usuario: usuarioAux.id
-        }
+    var cuentaAux = await cuenta.findOne({
+      where: {
+        id_usuario: usuarioAux.id
+      }
     })
     if (req.body.hasOwnProperty("estado")) {
 
-        cuentaAux.estado = req.body.estado;
-        await cuentaAux.save();
+      cuentaAux.estado = req.body.estado;
+      await cuentaAux.save();
 
+      res.status(200).json({ msg: "OK", code: 200 });
 
     } else {
-        res.status(400);
-        res.json({
-          msg: "ERROR",
-            tag: "Ingrese el estado de la cuenta",
-          code: 400
-        });    
+      res.status(400).json({ msg: "ERROR", tag: "Ingrese el estado de la cuenta", code: 400 });
     }
 
-    res.status(200);
-    res.json({
-      msg: "OK",
-      code: 200,
-      datos: cuentaAux,
-    });
+
   }
 
-  async cambiar_Clave(req, res) {
+  async cambiar_clave(req, res) {
     const external_usuario = req.params.external;
     const claveCifrada = await bcrypt.hash(req.body.clave, 10);
 
@@ -72,42 +46,36 @@ class CuentaControl {
         external_id: external_usuario,
       }
     });
-    var cuentaAux=await cuenta.findOne({
-        where: {
-            id_usuario: usuarioAux.id
-        }
+    var cuentaAux = await cuenta.findOne({
+      where: {
+        id_usuario: usuarioAux.id
+      }
     })
     //console.log(cuentaAux)
     if (req.body.hasOwnProperty("clave")) {
 
-        cuentaAux.clave = claveCifrada;
-        await cuentaAux.save();
+      cuentaAux.clave = claveCifrada;
+      await cuentaAux.save();
 
+      res.status(200).json({ msg: "OK", code: 200 });
 
     } else {
-        res.status(200);
-        res.json({
-          msg: "ERROR",
-            tag: "Ingrese la nueva clave",
-          code: 400
-        });    
+      res.status(200);
+      res.json({
+        msg: "ERROR",
+        tag: "Ingrese la nueva clave",
+        code: 400
+      });
     }
-
-    res.status(200);
-    res.json({
-      msg: "OK",
-      code: 200,
-      datos: cuentaAux,
-    });
   }
 
-  async validarInicio_Sesion(req, res, next){
+  async validarInicio_Sesion(req, res, next) {
     await check("nombre_usuario").notEmpty().withMessage("El usuario no debe estar vacio").run(req);
     await check("clave").notEmpty().withMessage("La clave no puede estar vacio").run(req);
 
     const errors = validationResult(req).formatWith(({ msg, value }) => ({ msg, value }));
     //console.log(errors.formatWith(msg, value))
-  
+
     if (!errors.isEmpty()) {
       return res.status(400).json({
         msg: "ERROR",
@@ -116,10 +84,10 @@ class CuentaControl {
         errors: errors.array(),
       });
     }
-  
+
     next();
   }
-  
+
   async inicio_sesion(req, res) {
     if (
       req.body.hasOwnProperty("nombre_usuario") &&
