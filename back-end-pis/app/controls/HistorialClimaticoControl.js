45513edd9e5a1663c28d6c3fe85a50ad9334378a
IvanG0nzalez <ivanalegonzalez3@gmail.com>
2024-01-30 21:14:55 -0500

    Arreglo fecha y hora Historial

--- back-end-pis/app/controls/HistorialClimaticoControl.js
+++ back-end-pis/app/controls/HistorialClimaticoControl.js
@@ -143,7 +143,7 @@ class HistorialControl {
     }
 
     async guardarAutomaticamente() {
-        try {
+        try { 
             const sensorAux = await sensor.findOne({
                 where: { alias: "Atmosferica" },
                 attributes: ['ip'],
@@ -182,8 +182,8 @@ class HistorialControl {
             });
 
             const fechaHoraActual = new Date();
-            const fechaActual = fechaHoraActual.toISOString().slice(0, 10);
-            const horaActual = fechaHoraActual.getHours() + ":" + fechaHoraActual.getMinutes() + ":" + fechaHoraActual.getSeconds();
+            const fechaActual = fechaHoraActual.toLocaleDateString('es-ES', { year: 'numeric', month: '2-digit', day: '2-digit' });
+            const horaActual = fechaHoraActual.toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit', second: '2-digit'});
 
             const dataTemperatura = {
                 fecha: fechaActual,
